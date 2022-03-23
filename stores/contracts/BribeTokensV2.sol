2852c355dce90586f03fed309332ec8c4840d511
Jetbrains <jetbrains10@protonmail.com>
2022-03-23 01:52:09 +0100

    Added constructor so the contract can be configured on deployment

--- stores/contracts/BribeTokensV2.sol
+++ stores/contracts/BribeTokensV2.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2021-09-03
-*/
-
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
@@ -53,9 +49,15 @@ interface bribe {
 contract BribeV2 {
     uint constant WEEK = 86400 * 7;
     uint constant PRECISION = 10**18;
-    GaugeController constant GAUGE = GaugeController(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
-    ve constant VE = ve(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
-    bribe constant BRIBE = bribe(0x7893bbb46613d7a4FbcC31Dab4C9b823FfeE1026);
+    GaugeController GAUGE;
+    ve VE;
+    bribe BRIBE;
+
+    constructor(address _gaugeControllerAddress, address _veAddress, address _bribeAddress){
+        GAUGE = GaugeController(_gaugeControllerAddress);
+        VE = ve(_veAddress);
+        BRIBE = bribe(_bribeAddress);
+    }
 
     function tokens_for_bribe(address user, address gauge, address reward_token) external view returns (uint) {
         uint _reward_per_token = BRIBE.reward_per_token(gauge, reward_token);
