8862f10530a56233f34a9e6b3623ca9483c96d5c
Jetbrains <jetbrains10@protonmail.com>
2022-04-15 03:30:21 +0500

    show CRV without click event

--- pages/rewards/index.js
+++ pages/rewards/index.js
@@ -284,9 +284,10 @@ function Voting({ changeTheme, theme }) {
           <div className={classes.copyContainer}>
             <div className={classes.copyCentered}>
               <Typography variant='h1' className={classes.titleSpacing}><span className={classes.helpingUnderline}>
-                {/* <button className={classes.platFormChangeButton} onClick={onPlatformClicked}>{platform}
-                </button> 
-                &nbsp;*/}Vote Incentives</span></Typography>
+                {/*onClick -> onPlatformClicked */}
+                <button className={classes.platFormChangeButton} onClick={() => {}}>{platform}
+                </button>
+                &nbsp;Vote Incentives</span></Typography>
               <Typography variant='h2' className={classes.helpingParagraph}>Get more for your votes! <ThumbUpIcon className={classes.thumbIcon} /></Typography>
               <div className={classes.divider}>
               </div>
